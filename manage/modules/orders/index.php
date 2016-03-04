27ada9ed58144888024bf5779c61ecc62dee8930
Igor Lashyn <ilashyn@gmail.com>
2016-03-04 09:01:29 +0200

    refs #653
    quick search by enter click

--- manage/modules/orders/index.php
+++ manage/modules/orders/index.php
@@ -601,7 +601,9 @@ class orders
         $orders_html .= '<li class=""><a class="click_tab btn btn-primary" href="'.$link.'#show_orders-sold" title="" onclick="click_tab(this, event)" data-open_tab="show_orders_sold"><i class="fa fa-money"></i> ' . l('ПРОДАЖИ') . '</a></li>';
         $orders_html .= '<li class=""><a class="click_tab btn btn-danger" href="'.$link.'#show_orders-writeoff" title="" onclick="click_tab(this, event)" data-open_tab="show_orders_writeoff"><i class="fa fa-times"></i> ' . l('СПИСАНИЯ') . '</a></li>';
         $orders_html .= '<li class=""><button data-toggle="filters" type="button" class="toggle-hidden btn btn-default"><i class="fa fa-filter"></i>' . l('Фильтровать') . '<i class="fa fa-caret-down"></i></button></li>';
-        $orders_html .= '<li style="max-width:280px"><div style="margin-bottom:-14px" class="input-group">
+        $orders_html .= '<li style="max-width:280px">
+                    <form method="POST" class="form-inline" onsubmit="return false;">
+                    <div class="input-group">
                             <span class="input-group-btn">
                                 <a href="'.$this->all_configs['prefix'].'orders" class="btn btn-default drop-quick-orders-serach" type="button">
                                     <i class="glyphicon glyphicon-remove-circle"></i>
@@ -609,7 +611,7 @@ class orders
                               </span>
                             <input type="text" value="'.(isset($_GET['qsq'])?htmlspecialchars($_GET['qsq']):'').'" name="search" class="form-control" id="orders_quick_search_query">
                             <div class="input-group-btn">
-                                  <button type="button"  onclick="orders_quick_search(this, \'simple\')" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">
+                                  <button type="submit"  onclick="orders_quick_search(this, \'simple\')" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">
                                     '.l('Искать').'
                                   </button>
                                 <div class="btn-group orders_quick_search_dropdown dropdown">
@@ -628,7 +630,10 @@ class orders
                                   </ul>
                                 </div>
                             </div>
-                        </div></li>';
+
+                        </div>
+                        </form>
+                        </li>';
         if ($this->all_configs['oRole']->hasPrivilege('create-clients-orders')) {
             $orders_html .= '<li class="pull-right"><a href="' . $this->all_configs['prefix'] . 'orders/#create_order" class="btn btn-success hash_link">' . l('Создать заказ') . '</a></li>';
         }
