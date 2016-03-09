d14ea8e672ffc71ace62e04e452b0fe746b64869
Igor Lashyn <ilashyn@gmail.com>
2016-03-09 12:58:38 +0200

    refs #729
    reformat
    fix url for load widget

--- widget.php
+++ widget.php
@@ -6,15 +6,15 @@ $active_widgets = array(
 
 // загружаем виджет
 $name = isset($_GET['w']) ? trim($_GET['w']) : null;
-if($name){
+if ($name) {
     $script = '';
-    if(in_array($name, $active_widgets)){
-        include __DIR__.'/manage/inc_config.php';
-        include __DIR__.'/manage/inc_func.php';
-        include __DIR__.'/manage/inc_settings.php';
-        $widget = get_service('widgets/'.$name);
-        if(!is_null($widget) && method_exists($widget, 'load_widget')){
-            $widgets_service = get_service('widgets');     
+    if (in_array($name, $active_widgets)) {
+        include __DIR__ . '/manage/inc_config.php';
+        include __DIR__ . '/manage/inc_func.php';
+        include __DIR__ . '/manage/inc_settings.php';
+        $widget = get_service('widgets/' . $name);
+        if (!is_null($widget) && method_exists($widget, 'load_widget')) {
+            $widgets_service = get_service('widgets');
             $has_jquery = isset($_GET['jquery']) && $_GET['jquery'] == 1;
             $script .= $widgets_service->load_widget_service($has_jquery);
             $script .= $widget->load_widget();
@@ -28,21 +28,21 @@ if($name){
 
 // принимаем запросы
 $is_ajax = isset($_GET['ajax']);
-if($is_ajax){
+if ($is_ajax) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST,GET');
-    if(isset($HTTP_RAW_POST_DATA)){
+    if (isset($HTTP_RAW_POST_DATA)) {
         parse_str($HTTP_RAW_POST_DATA, $post);
-    }else{
+    } else {
         $post = $_POST;
     }
     $widget_name = isset($post['widget']) ? trim($post['widget']) : null;
-    if(in_array($widget_name, $active_widgets)){
-        include __DIR__.'/manage/inc_config.php';
-        include __DIR__.'/manage/inc_func.php';
-        include __DIR__.'/manage/inc_settings.php';
-        $widget = get_service('widgets/'.$widget_name);
-        if(!is_null($widget) && method_exists($widget, 'ajax')){
+    if (in_array($widget_name, $active_widgets)) {
+        include __DIR__ . '/manage/inc_config.php';
+        include __DIR__ . '/manage/inc_func.php';
+        include __DIR__ . '/manage/inc_settings.php';
+        $widget = get_service('widgets/' . $widget_name);
+        if (!is_null($widget) && method_exists($widget, 'ajax')) {
             $response = $widget->ajax($post);
             header('Content-type: application/json; charset=utf-8');
             echo json_encode($response);
