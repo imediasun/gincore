d10f0c208f1704e61e88ef05a5b2bb7cc8e8265d
Igor Lashyn <ilashyn@gmail.com>
2016-03-30 12:48:52 +0300

    refs #703
    test script for erp-v2

--- feedback.php
+++ feedback.php
@@ -8,29 +8,13 @@
             var s = document.createElement("script");
             s.type = "text/javascript";
             s.async = true;
-            s.src = "//localhost:8080/widget.php?ajax=&w=status&jquery="+("jQuery" in window?1:0);
-            if(document.getElementsByClassName('gcw-fixed').length > 0) {
-                document.getElementsByClassName('gcw-fixed')[0].appendChild(s);
-            } else {
-                var fixed = document.createElement('div');
-                fixed.className = 'gcw-fixed';
-                fixed.appendChild(s);
-                document.getElementsByTagName("head")[0].appendChild(fixed);
-            }
-        })();
-    </script>
-    <script>
-        (function () {
-            var s = document.createElement("script");
-            s.type = "text/javascript";
-            s.async = true;
-            s.src = "//localhost:8080/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
+            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
             document.getElementsByTagName("head")[0].appendChild(s);
         })();
     </script>
     <style>
-        .gcw_title {
-            background-color: white !important;
+        .gcw_feedback_modal .gcw_title {
+            background-color: #b3c7e1  !important;
         }
     </style>
 </head>
