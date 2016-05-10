f799dfc71ed79ab016c6547219bc409c4cc3ee79
Igor Lashyn <ilashyn@gmail.com>
2016-05-10 09:09:46 +0300

    refs #943
    load from erpv2

--- feedback.php
+++ feedback.php
@@ -8,8 +8,8 @@
             var s = document.createElement("script");
             s.type = "text/javascript";
             s.async = true;
-//            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
-            s.src = "//localhost:8080/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
+            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
+//            s.src = "//localhost:8080/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
             document.getElementsByTagName("head")[0].appendChild(s);
         })();
     </script>
@@ -18,8 +18,8 @@
             var s = document.createElement("script");
             s.type = "text/javascript";
             s.async = true;
-//            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
-            s.src = "//localhost:8080/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
+            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
+//            s.src = "//localhost:8080/widget.php?w=feedback&jquery="+(typeof jQuery != 'undefined'?1:0);
             document.getElementsByTagName("head")[0].appendChild(s);
         })();
     </script>
@@ -28,8 +28,8 @@
             var s = document.createElement("script");
             s.type = "text/javascript";
             s.async = true;
-//            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
-            s.src = "//localhost:8080/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
+            s.src = "//erp.mac-pro.com.ua/version2/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
+//            s.src = "//localhost:8080/widget.php?w=status&jquery="+(typeof jQuery != 'undefined'?1:0);
             document.getElementsByTagName("head")[0].appendChild(s);
         })();
     </script>
