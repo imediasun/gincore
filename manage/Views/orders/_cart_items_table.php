c4f02e02a356ef0904de8852aa7f31b965c3d3ad
Igor Lashyn <ilashyn@gmail.com>
2016-05-16 14:26:38 +0300

    refs #913
    исправлена верстка корзин

--- manage/Views/orders/_cart_items_table.php
+++ manage/Views/orders/_cart_items_table.php
@@ -9,56 +9,52 @@
                 <?php if ($prefix == 'eshop'): ?>
                     <th><?= l('Количество') ?></th>
                 <?php endif; ?>
-                <th><?= l('Сумма') ?>(<?= viewCurrency() ?>)</th>
+                    <th><?= l('Сумма') ?>(<?= viewCurrency() ?>)</th>
+                <?php if ($prefix != 'eshop'): ?>
                 <th class="<?= $prefix == 'quick' ? 'col-sm-3' : '' ?>"><?= l('Гарантия') ?></th>
+                <?php endif; ?>
                 <th></th>
             </tr>
             </thead>
             <tbody>
             <tr class="js-<?= $prefix ?>-row-cloning" style="display: none">
                 <td>
-                    <div class="input-group col-sm-12">
                         <input type="hidden" class="form-control js-<?= $prefix ?>-item-id" name="" value="">
                         <input type="text" readonly class="form-control js-<?= $prefix ?>-item-name" value=""/>
-                    </div>
                 </td>
                 <td>
-                    <div class="input-group col-sm-12">
                         <input type="text" class="form-control js-<?= $prefix ?>-price"
                                onkeyup="recalculate_amount_<?= $prefix ?>();" value="" name=""/>
-                    </div>
                 </td>
                 <td>
-                    <div class="input-group col-sm-12">
-                        <input type="text" class="form-control js-<?= $prefix ?>-discount" onkeyup="recalculate_amount_<?= $prefix ?>();"  value="0"/>
+                        <input type="text" class="form-control js-<?= $prefix ?>-discount"
+                               onkeyup="recalculate_amount_<?= $prefix ?>();" value="0"/>
                         <input type="hidden" class="form-control js-<?= $prefix ?>-discount_type" value="1"/>
-                    </div>
                 </td>
                 <?php if ($prefix == 'eshop'): ?>
                     <td>
-                        <div class="input-group col-sm-12">
-                            <input type="text" class="form-control js-<?= $prefix ?>-quantity" onkeyup="recalculate_amount_<?= $prefix ?>();" value=""/>
-                        </div>
+                            <input type="text" class="form-control js-<?= $prefix ?>-quantity"
+                                   onkeyup="recalculate_amount_<?= $prefix ?>();" value=""/>
                     </td>
                 <?php endif; ?>
                 <td>
-                    <div class="input-group col-sm-12">
                         <input type="text" class="form-control js-<?= $prefix ?>-sum dasabled" readonly
                                onkeyup="recalculate_amount_<?= $prefix ?>(this);" value="" name=""/>
-                    </div>
                 </td>
-                <td>
-                    <div class="input-group col-sm-12">
-                        <select class="form-control js-<?= $prefix ?>-warranty" name="">
-                            <option value=""><?= l('Без гарантии') ?></option>
-                            <?php foreach ($orderWarranties as $warranty): ?>
-                                <option
-                                    value="<?= intval($warranty) ?>" <?= $warranty == $defaultWarranty ? 'selected="selected"' : '' ?>><?= intval($warranty) ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                        <div class="input-group-addon"><?= l('мес') ?></div>
-                    </div>
-                </td>
+                <?php if ($prefix != 'eshop'): ?>
+                    <td>
+                        <div class="input-group col-sm-12">
+                            <select class="form-control js-<?= $prefix ?>-warranty" name="">
+                                <option value=""><?= l('Без гарантии') ?></option>
+                                <?php foreach ($orderWarranties as $warranty): ?>
+                                    <option
+                                        value="<?= intval($warranty) ?>" <?= $warranty == $defaultWarranty ? 'selected="selected"' : '' ?>><?= intval($warranty) ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <div class="input-group-addon"><?= l('мес') ?></div>
+                        </div>
+                    </td>
+                <?php endif; ?>
                 <td>
                     <a href="#" onclick="return remove_row_<?= $prefix ?>(this);">
                         <i class="glyphicon glyphicon-remove"></i>
@@ -69,26 +65,25 @@
             <tfoot>
             <tr class="row-amount">
                 <td>
-                    <div class="input-group col-sm-12">
                         <label><?= l('Итоговая стоимость:') ?></label>
-                    </div>
                 </td>
                 <td></td>
                 <?php if ($prefix == 'eshop'): ?>
                     <td></td>
+                    <td>
+                            <input type="checkbox" name="cashless" class="cashless-toggle" title="<?= l('Отфильтровать все безналичные счета для сверки Вы можете в разделе: Бухгалтерия-Заказы-Заказы клиентов') ?>">
+                    </td>
+                <?php else: ?>
+                    <td></td>
                 <?php endif; ?>
-                <td></td>
                 <td>
-                    <div class="input-group col-sm-12">
                         <input type="text" readonly class="form-control js-<?= $prefix ?>-total" value=""/>
-                    </div>
                 </td>
-                <td>
-                    <div class="input-group"
-                         title="<?= l('Отфильтровать все безналичные счета для сверки Вы можете в разделе: Бухгалтерия-Заказы-Заказы клиентов') ?>">
-                        <input type="checkbox" name="cashless" class="cashless-toggle">
-                    </div>
-                </td>
+                <?php if ($prefix != 'eshop'): ?>
+                    <td>
+                            <input type="checkbox" name="cashless" class="cashless-toggle" title="<?= l('Отфильтровать все безналичные счета для сверки Вы можете в разделе: Бухгалтерия-Заказы-Заказы клиентов') ?>">
+                    </td>
+                <?php endif; ?>
                 <td></td>
             </tr>
             </tfoot>
