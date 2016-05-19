547d3fc0b297ef84df9b89ee4c18dc765d7283fb
Igor Lashyn <ilashyn@gmail.com>
2016-05-19 16:12:56 +0300

    refs #913
    format table

--- manage/Views/prints/waybill_products.php
+++ manage/Views/prints/waybill_products.php
@@ -1,23 +1,23 @@
-<table>
+<table class="table" style="font: 12px;">
     <thead>
-    <tr>
-        <td>
+    <tr style="font-weight: bold; text-align: center">
+        <td style="border: 1px solid grey">
             <?= l('N п/п') ?>
         </td>
-        <td>
+        <td style="border: 1px solid grey">
             <?= l('Наименование') ?>
         </td>
-        <td>
+        <td style="border: 1px solid grey">
             <?= l('Количество') ?>
         </td>
-        <td>
+        <td style="border: 1px solid grey">
             <?= l('Цена') ?><br>
             <?= viewCurrency() ?>
         </td>
-        <td>
+        <td style="border: 1px solid grey">
             <?= l('Скидка') ?>
         </td>
-        <td>
+        <td style="border: 1px solid grey">
             <?= l('Сумма') ?><br>
             <?= viewCurrency() ?>            
         </td>
@@ -29,22 +29,22 @@
         <?php $i = 1; ?>
         <?php foreach ($goods as $good): ?>
             <tr>
-                <td>
+                <td style="border: 1px solid grey">
                     <?= $i++ ?>
                 </td>
-                <td>
+                <td style="border: 1px solid grey">
                     <?= h($good['title']) ?>
                 </td>
-                <td>
+                <td style="border: 1px solid grey; text-align: right; padding-right: 30px !important;">
                     <?= h($good['count']) ?>
                 </td>
-                <td>
+                <td style="border: 1px solid grey; text-align: right; padding-right: 30px !important;">
                     <?= h($good['price']) / 100 ?>
                 </td>
-                <td>
+                <td style="border: 1px solid grey; text-align: right; padding-right: 30px !important;">
                     <?= h($good['discount']) ?> <?= $good['discount_type'] == DISCOUNT_TYPE_PERCENT ? '%' : viewCurrency() ?>
                 </td>
-                <td>
+                <td style="border: 1px solid grey; text-align: right; padding-right: 30px !important;">
                     <?= sum_with_discount($good) ?>
                 </td>
             </tr>
