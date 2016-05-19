8d67198a769413a715f63cace4d1f5bc4c98b540
Igor Lashyn <ilashyn@gmail.com>
2016-05-19 16:04:02 +0300

    refs #913
    add title to production list

--- manage/Views/prints/waybill_products.php
+++ manage/Views/prints/waybill_products.php
@@ -1,25 +1,54 @@
-<?php if (!empty($goods)): ?>
-    <?php $i = 1; ?>
-    <?php foreach ($goods as $good): ?>
-        <tr>
-            <td>
-                <?= $i++ ?>
-            </td>
-            <td>
-                <?= h($good['title']) ?>
-            </td>
-            <td>
-                <?= h($good['count']) ?>
-            </td>
-            <td>
-                <?= h($good['price'])/100 ?>
-            </td>
-            <td>
-                <?= h($good['discount']) ?>
-            </td>
-            <td>
-                <?= sum_with_discount($good) ?>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-<?php endif; ?>
+<table>
+    <thead>
+    <tr>
+        <td>
+            <?= l('N п/п') ?>
+        </td>
+        <td>
+            <?= l('Наименование') ?>
+        </td>
+        <td>
+            <?= l('Количество') ?>
+        </td>
+        <td>
+            <?= l('Цена') ?><br>
+            <?= viewCurrency() ?>
+        </td>
+        <td>
+            <?= l('Скидка') ?>
+        </td>
+        <td>
+            <?= l('Сумма') ?><br>
+            <?= viewCurrency() ?>            
+        </td>
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php if (!empty($goods)): ?>
+        <?php $i = 1; ?>
+        <?php foreach ($goods as $good): ?>
+            <tr>
+                <td>
+                    <?= $i++ ?>
+                </td>
+                <td>
+                    <?= h($good['title']) ?>
+                </td>
+                <td>
+                    <?= h($good['count']) ?>
+                </td>
+                <td>
+                    <?= h($good['price']) / 100 ?>
+                </td>
+                <td>
+                    <?= h($good['discount']) ?> <?= $good['discount_type'] == DISCOUNT_TYPE_PERCENT ? '%' : viewCurrency() ?>
+                </td>
+                <td>
+                    <?= sum_with_discount($good) ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    </tbody>
+</table>
