4b3a7f585b53de6e38167d88b32f09d283281454
Igor Lashyn <ilashyn@gmail.com>
2016-05-20 16:34:37 +0300

    Merge branch 'feature/913-eshop' into feature/907-piece-wage
    * feature/913-eshop:
      Merge branch 'develop' into feature/913-eshop
      resf #952
      resf #952
      задачи 828, 860, 861, 866(частично, нужно еще проверять), 888
      refs #932
    
    Conflicts:
    	manage/Views/accountings/reports_turnover/filters.php
    	manage/Views/accountings/reports_turnover/unloading.php
    	manage/modules/accountings/css/main.css

--- manage/InfoPopover.php
+++ manage/InfoPopover.php
@@ -1,107 +1,144 @@
 <?php
 
-class InfoPopover{
-    
+class InfoPopover
+{
+
     private static $instance = null;
-    
+
     private $all_configs;
+    /** @var  View */
     private $view;
-    
+
+    /**
+     * @param $text_var
+     * @return mixed
+     */
     public function createQuestion($text_var)
     {
-        $text = l($text_var);
         return $this->view->renderFile('InfoPopover/question', array(
-                   'content' => $text
-               ));
+            'content' => l($text_var)
+        ));
     }
-    
+
+    /**
+     * @param $text_var
+     * @return mixed
+     */
     public function createOnHoverAttr($text_var)
     {
-        $text = l($text_var);
         return $this->view->renderFile('InfoPopover/onHoverAttr', array(
-                   'content' => $text
-               ));
+            'content' => l($text_var)
+        ));
     }
-    
+
+    /**
+     * @param      $text_var
+     * @param bool $oneTime
+     * @return string
+     */
     public function createOnLoad($text_var, $oneTime = true)
     {
-        if($oneTime && !$this->oneTimePopoverEnabled($text_var)){
+        if ($oneTime && !$this->oneTimePopoverEnabled($text_var)) {
             return '';
-        }else{
-            $text = l($text_var);
-            return $this->view->renderFile('InfoPopover/onLoad'.($oneTime?'OneTime':''), array(
-                'content' => $text,
-                'id' => $text_var
-            ));
         }
+        return $this->view->renderFile('InfoPopover/onLoad' . ($oneTime ? 'OneTime' : ''), array(
+            'content' => l($text_var),
+            'id' => $text_var
+        ));
     }
-    
+
+    /**
+     * @param      $text_var
+     * @param bool $has_confirm
+     * @return string
+     */
     public function createInfoModal($text_var, $has_confirm = true)
     {
-        if($has_confirm && !$this->oneTimePopoverEnabled($text_var)){
+        if ($has_confirm && !$this->oneTimePopoverEnabled($text_var)) {
             return '';
-        }else{
-            $text = l($text_var);
-            return $this->view->renderFile('InfoPopover/infoModal', array(
-                'content' => $text,
-                'id' => $text_var
-            ));
         }
+        return $this->view->renderFile('InfoPopover/infoModal', array(
+            'content' => l($text_var),
+            'id' => $text_var
+        ));
     }
-    
-    private function getPopoverSettings(){
-        $s = !empty($this->all_configs['settings']['info_popovers_settings']) 
-                ? json_decode($this->all_configs['settings']['info_popovers_settings'], true) 
-                    : array();
-        return $s;
+
+    /**
+     * @return array|mixed
+     */
+    private function getPopoverSettings()
+    {
+        return !empty($this->all_configs['settings']['info_popovers_settings'])
+            ? json_decode($this->all_configs['settings']['info_popovers_settings'], true)
+            : array();
     }
-    
+
+    /**
+     * @param $text_var
+     * @return bool
+     */
     private function oneTimePopoverEnabled($text_var)
     {
         $settings = $this->getPopoverSettings();
         return empty($settings[$text_var]);
     }
-    
+
+    /**
+     * @param      $text_var
+     * @param bool $hide
+     * @return bool
+     */
     public function oneTimePopoverToggle($text_var, $hide = true)
     {
-        if(!$this->popoverExists($text_var)){
+        if (!$this->popoverExists($text_var)) {
             return false;
         }
         $settings = $this->getPopoverSettings();
-        if($hide){
+        if ($hide) {
             $settings[$text_var] = 1;
-        }elseif(isset($settings[$text_var])){
+        } elseif (isset($settings[$text_var])) {
             unset($settings[$text_var]);
         }
         $json = json_encode($settings);
         $this->all_configs['db']->query("INSERT INTO {settings}(name,value,ro) "
-                                       ."VALUES('info_popovers_settings',?:settings,1) "
-                                       ."ON DUPLICATE KEY UPDATE value = ?:settings", array(
-                                           'settings' => $json
-                                       ));
+            . "VALUES('info_popovers_settings',?:settings,1) "
+            . "ON DUPLICATE KEY UPDATE value = ?:settings", array(
+            'settings' => $json
+        ));
         $this->all_configs['settings']['info_popovers_settings'] = $json;
     }
-        
+
+    /**
+     * @param $text_var
+     * @return bool
+     */
     public function popoverExists($text_var)
     {
         return isset($this->manage_translates[$text_var]);
     }
-    
-    private function setConfigs($all_configs, $manage_translates)
+
+    /**
+     * @return InfoPopover|null
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            global $all_configs, $manage_translates;
+            self::$instance = new self($all_configs, $manage_translates);
+        }
+        return self::$instance;
+    }
+
+    /**
+     * InfoPopover constructor.
+     * @param $all_configs
+     * @param $manage_translates
+     */
+    private function __construct(&$all_configs, $manage_translates)
     {
         $this->all_configs = $all_configs;
         $this->manage_translates = $manage_translates;
         $this->view = new View($this->all_configs);
+
     }
-    
-    public static function getInstance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
-            global $all_configs, $manage_translates;
-            self::$instance->setConfigs($all_configs, $manage_translates);
-        }
-        return self::$instance;
-    }
-    private function __construct(){}
 }
