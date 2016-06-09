779fda90cc895bb7257adea09b42a1d564379ae3
Igor Lashyn <ilashyn@gmail.com>
2016-06-09 12:04:55 +0300

    not show price list if printed location

--- manage/Views/warehouses/print_buttons.php
+++ manage/Views/warehouses/print_buttons.php
@@ -1,41 +1,47 @@
 <div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle dropdown-btn<?= $prefix ?>" data-toggle="dropdown" aria-haspopup="true"
+    <button type="button" class="btn btn-default dropdown-toggle dropdown-btn<?= $prefix ?>" data-toggle="dropdown"
+            aria-haspopup="true"
             aria-expanded="false">
         <i class="fa fa-print"></i> <span class="caret"></span>
     </button>
     <ul class="keep-open dropdown-menu print_menu">
-        <li>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="print[]"
-                           value="<?= print_link($objectId, 'label' . $prefix, '', true, !empty($addition)? $addition: '') ?>">
-                    <?= l('Этикетка (штрих-код)') ?>
-                </label>
-            </div>
-        </li>
-        <li>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="print[]"
-                           value="<?= print_link($objectId, 'price_list' . $prefix, '', true, !empty($addition)? $addition: '') ?>">
-                    <?= l('Ценник') ?>
-                </label>
-            </div>
-        </li>
         <?php if ($prefix == '_location'): ?>
             <li>
                 <div class="checkbox">
                     <label>
                         <input type="checkbox" name="print[]"
-                               value="<?= print_link($objectId, 'location', '', true, !empty($addition)? $addition: '') ?>">
+                               value="<?= print_link($objectId, 'location', '', true,
+                                   !empty($addition) ? $addition : '') ?>">
                         <?= l('Локация') ?>
+                    </label>
+                </div>
+            </li>
+        <?php else: ?>
+            <li>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="print[]"
+                               value="<?= print_link($objectId, 'label' . $prefix, '', true,
+                                   !empty($addition) ? $addition : '') ?>">
+                        <?= l('Этикетка (штрих-код)') ?>
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="print[]"
+                               value="<?= print_link($objectId, 'price_list' . $prefix, '', true,
+                                   !empty($addition) ? $addition : '') ?>">
+                        <?= l('Ценник') ?>
                     </label>
                 </div>
             </li>
         <?php endif; ?>
         <li role="separator" class="divider"></li>
         <li class="text-center">
-            <button class="btn btn-sm btn-info print_now" type="button" onclick="return print_now(this);"><?= l('Распечатать') ?></button>
+            <button class="btn btn-sm btn-info print_now" type="button"
+                    onclick="return print_now(this);"><?= l('Распечатать') ?></button>
         </li>
     </ul>
 </div>
