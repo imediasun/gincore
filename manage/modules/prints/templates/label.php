a5776401e02c7036d6cb82756914a8797ebc34a6
Igor Lashyn <ilashyn@gmail.com>
2016-06-20 13:42:11 +0300

    correct print label for item

--- manage/modules/prints/templates/label.php
+++ manage/modules/prints/templates/label.php
@@ -9,7 +9,7 @@ class label extends AbstractTemplate
         $product = $this->all_configs['db']->query('SELECT g.barcode, g.title, i.serial, i.id as item_id,
                   o.number, o.parent_id, o.id, o.num
                 FROM {goods} as g, {warehouses_goods_items} as i, {contractors_suppliers_orders} as o
-                WHERE i.goods_id=g.id AND i.id=?i AND o.id=i.supplier_order_id', array($object))->row();
+                WHERE i.goods_id=g.id AND g.id=?i AND o.id=i.supplier_order_id', array($object))->row();
 
         return $this->view->renderFile('prints/label', array(
             'product' => $product
