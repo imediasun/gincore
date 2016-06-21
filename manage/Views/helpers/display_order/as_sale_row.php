03db4e72d48efa55d4b0466b97ae94e4dce12fa7
Igor Lashyn <ilashyn@gmail.com>
2016-06-21 09:17:38 +0300

    refs #1007
    вернул звездочки, добавил фильтры по типу заказов

--- manage/Views/helpers/display_order/as_sale_row.php
+++ manage/Views/helpers/display_order/as_sale_row.php
@@ -1,5 +1,6 @@
 <tr class="remove-marked-object">
     <td class="floatleft">
+        <?= show_marked($order['order_id'], 'co', $order['m_id']) ?>
         <?php if ($this->all_configs['oRole']->hasPrivilege('edit-clients-orders') ||
             $this->all_configs['oRole']->hasPrivilege('show-clients-orders')
         ): ?>
