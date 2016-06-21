f18bd24216e51a68adbccb7531c5b37d64ba2d0e
Igor Lashyn <ilashyn@gmail.com>
2016-06-21 08:51:18 +0300

    исправил формирование штрихкодов для списка товаров

--- manage/Views/warehouses/print_buttons.php
+++ manage/Views/warehouses/print_buttons.php
@@ -21,7 +21,7 @@
                 <div class="checkbox">
                     <label>
                         <input type="checkbox" name="print[]"
-                               value="<?= print_link($whItemId, 'label' . $prefix, '', true,
+                               value="<?= print_link(isset($whItemId) ? $whItemId : $objectId, 'label' . $prefix, '', true,
                                    !empty($addition) ? $addition : '') ?>">
                         <?= l('Этикетка (штрих-код)') ?>
                     </label>
