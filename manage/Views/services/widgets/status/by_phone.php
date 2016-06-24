38b63f7a49939bb2b8c1316d01fab273f0d0f287
Igor Lashyn <ilashyn@gmail.com>
2016-06-24 16:03:04 +0300

    упрощаем

--- manage/Views/services/widgets/status/by_phone.php
+++ manage/Views/services/widgets/status/by_phone.php
@@ -1,14 +1,14 @@
 <?php if ($orders): ?>
     <?php foreach ($orders as $order): ?>
         <?php $status = isset($this->all_configs['configs']['order-status'][$order['status']])
-        ? htmlspecialchars($this->all_configs['configs']['order-status'][$order['status']]['name'])
+        ? h($this->all_configs['configs']['order-status'][$order['status']]['name'])
         : ''; ?>
         <div class="gcw_status_order">
             <h2><?= l('Ремонт') ?> №<?= $order['id'] ?></h2>
             <p><b><?= l('Дата') ?></b>: <?= date("d/m/Y", strtotime($order['date_add'])) ?></p>
             <p><b><?= l('Статус') ?></b>: <?= $status ?></p>
-            <p><b><?= l('Устройство') ?></b>: <?= htmlspecialchars($order['title']) ?></p>
-            <p><b><?= l('Серийный номер') ?></b>: <?= htmlspecialchars($order['serial']) ?></p>
+            <p><b><?= l('Устройство') ?></b>: <?= h($order['title']) ?></p>
+            <p><b><?= l('Серийный номер') ?></b>: <?= h($order['serial']) ?></p>
 
             <?php if ($order['comments']): ?>
                 <table class="gcw_table gcw_table_stripped">
@@ -26,7 +26,7 @@
                             <td>
                                 <center><?= date("d.m.Y<b\\r/>H:i", strtotime($comment['date_add'])) ?></center>
                             </td>
-                            <td><?= htmlspecialchars(wordwrap($comment['text'], 25, " ", true)) ?></td>
+                            <td><?= h(wordwrap($comment['text'], 25, " ", true)) ?></td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
