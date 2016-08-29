4d4fbf41230055b44ae16a029324e868341ba7ca
Igor Lashyn <ilashyn@gmail.com>
2016-08-29 18:00:19 +0300

    refs #1096
    исправление приходования после импорта

--- manage/modules/warehouses/index.php
+++ manage/modules/warehouses/index.php
@@ -2292,7 +2292,7 @@ class warehouses extends Controller
         $data = array(
             'user_id' => $this->getUserId(),
             'supplier_id' => $post['warehouse-supplier'],
-            'description' => $post['comment-supplier'],
+            'comment' => $post['comment-supplier'],
             'date' => date('Y-m-d H:i:s', strtotime($post['warehouse-order-date'])),
             'items' => $items,
             'type' => $post['warehouse-type'],
