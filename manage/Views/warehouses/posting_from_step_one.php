7efa6babe93d549d60f62ffd89678ff0cf7c28c0
Igor Lashyn <ilashyn@gmail.com>
2016-08-30 13:03:06 +0300

    refs #1096
    изменение стилей кнопок

--- manage/Views/warehouses/posting_from_step_one.php
+++ manage/Views/warehouses/posting_from_step_one.php
@@ -4,11 +4,11 @@
         <?= l('Создать заказ поставщику в разделе Заказы-Создать заказ поставщику. После чего в разделе Склад-Заказы поставщикам выбрать из списка нужный заказ и оприходовать его') ?>
     </li>
     <li style="margin-bottom: 20px">
-        <a href="<?= $this->all_configs['prefix'] ?>import?load=posting_items#import" class="btn btn-default"><?= l('Загрузить') ?></a>
+        <a href="<?= $this->all_configs['prefix'] ?>import?load=posting_items#import" class="btn btn-default btn-sm"><?= l('Загрузить') ?></a>
         <?= l('накладную от поставщика в формате Excel. Товар перечисленный в накладной будет оприходован на склад') ?>
     </li>
     <li>
-        <a href="" class="btn btn-default" onclick="return create_purchase_invoice();"><?= l('Создать') ?></a>
+        <a href="" class="btn btn-default btn-sm" onclick="return create_purchase_invoice();"><?= l('Создать') ?></a>
         <?= l('накладную самостоятельно') ?>
     </li>
 </ol>
