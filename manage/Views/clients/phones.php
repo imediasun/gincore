5535bb1dc1843cd1f04ebf5dc3e6abf6e1746150
Igor Lashyn <ilashyn@gmail.com>
2016-09-12 16:41:53 +0300

    refs #1237
    скрывает телефон

--- manage/Views/clients/phones.php
+++ manage/Views/clients/phones.php
@@ -1,9 +1,10 @@
 <?php if (!empty($phones)): ?>
     <?php foreach ($phones as $phone): ?>
-        <input<?=input_phone_mask_attr()?> class="form-control clone_clear_val" type="text" onkeydown="return isNumberKey(event)"
-               name="phone[]" value="<?= htmlspecialchars($phone) ?>"/>
+        <input <?= input_phone_mask_attr() ?> class="form-control" type="text"
+                                             onkeydown="return isNumberKey(event)"
+                                             name="phone[]" value="<?= h($phone) ?>"/>
     <?php endforeach; ?>
-<?php else: ?>
-    <input<?=input_phone_mask_attr()?> class="form-control clone_clear_val" type="text" onkeydown="return isNumberKey(event)"
-           name="phone[]" value=""/>
 <?php endif; ?>
+<input <?= input_phone_mask_attr() ?> class="form-control clone_clear_val" type="text"
+                                     onkeydown="return isNumberKey(event)"
+                                     name="phone[]" value=""/>
