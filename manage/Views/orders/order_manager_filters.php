1687a89dbec3a6019e8339bc985cc96052d3dda9
Igor Lashyn <ilashyn@gmail.com>
2016-09-14 16:41:42 +0300

    refs #1224
    фильтры в одну строку

--- manage/Views/orders/order_manager_filters.php
+++ manage/Views/orders/order_manager_filters.php
@@ -1,36 +1,32 @@
 <div>
     <form class="form-inline well">
-        <table class="table table-borderless" style="margin-bottom: 0px">
-            <tr>
-                <td>
-                    <?= $manager_filter ?>
-                </td>
-                <td>
-                    <?= $service_filter ?>
-                </td>
-                <td>
-                    <div class="input-group">
-                        <p class="form-control-static" style="display: inline-block; margin-right: 10px;">
-                            <?= l('Статистика за') ?>
-                        </p>
-                        <span class="input-group-btn">
+        <?= $manager_filter ?>
+        <?= $service_filter ?>
+        <div class="input-group">
+            <p class="form-control-static" style="display: inline-block; margin-right: 10px;">
+                <?= l('Статистика за') ?>
+            </p>
+            <span class="input-group-btn">
                         <input type="text" placeholder="<?= l('Дата') ?>" name="date"
                                class="daterangepicker form-control "
                                value="<?= $get_date ?>"/>
                         </span>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <input type="submit" class="btn btn-primary" value="<?= l('Фильтровать') ?>">
-                    <button type="button" class="btn fullscreen"><i class="fa fa-arrows-alt"></i></button>
-                    <button type="button" class="btn btn-primary"
-                            onclick="return manager_setup(this);"><?= l('Настройки') ?></button>
-                </td>
-            </tr>
-        </table>
+        </div>
+        <div class="input-group" style="margin-right: 0">
+            <input type="submit" class="btn btn-primary" value="<?= l('Фильтровать') ?>">
+            <button type="button" class="btn fullscreen"><i class="fa fa-arrows-alt"></i></button>
+            <button type="button" class="btn btn-primary"
+                    onclick="return manager_setup(this);"><?= l('Настройки') ?></button>
+        </div>
     </form>
 </div>
 <?= $filter_stats ?>
 <br>
+<style>
+    input[name='date'] {
+        width: 170px
+    }
+    .form-inline .input-group {
+        margin: 5px 5px 5px 0;
+    }
+</style>
