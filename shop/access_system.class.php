11793287ac1f47867fdebfa78dfb285479f79da6
Igor Lashyn <ilashyn@gmail.com>
2016-09-21 09:44:57 +0300

    refs #1153
    убрал логины из списка менеджеров

--- shop/access_system.class.php
+++ shop/access_system.class.php
@@ -75,7 +75,7 @@ class Role
         $arr = (array)$arr;
         $query = $all == self::ALL ? '' : $this->all_configs['db']->makeQuery('AND u.avail=1 AND u.deleted=0', array());
         if (count($arr) > 0) {
-            $users = (array)$this->all_configs['db']->query('SELECT u.*, CONCAT(u.fio, " ", u.login) as name
+            $users = (array)$this->all_configs['db']->query('SELECT u.*, if(u.fio is NULL || u.fio = "", u.login, u.fio) as name
                 FROM {users} as u, {users_permissions} as p, {users_role_permission} as l
                 WHERE p.link IN (?l) AND u.role=l.role_id AND l.permission_id=p.id ?query',
                 array($arr, $query))->assoc('id');
