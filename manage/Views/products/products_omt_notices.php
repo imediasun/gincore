cfdab69b8fcc8f8baf697624e1c5f4e87e046c7c
Igor Lashyn <ilashyn@gmail.com>
2016-09-27 13:53:34 +0300

    refs #1260
    изменение верстки

--- manage/Views/products/products_omt_notices.php
+++ manage/Views/products/products_omt_notices.php
@@ -1,8 +1,8 @@
-<form method="post" style="max-width:400px">
+<form method="post" style="max-width:550px">
     <table class="table table-borderless">
         <tbody>
         <tr>
-            <td colspan="2">
+            <td colspan="3">
                 <div class="checkbox">
                     <label>
                         <input <?= ($user && $user['each_sale'] == 1) ? 'checked' : '' ?> type="checkbox" name="each_sale"/>
@@ -18,12 +18,12 @@
                     <?= l('уведомлять меня об остатке') ?>
                 </label>
             </td>
-            <td>
+            <td colspan="2">
                 <div class="input-group">
                     <input placeholder="<?= l('количество товаров') ?>"
                            value="<?= ($user && $user['balance'] > 0) ? $user['balance'] : ''; ?>"
                            type="text" class="form-control" onkeydown="return isNumberKey(event)"
-                           name="balance"/>
+                           name="balance" style="width: 150px;"/>
                     <div class="input-group-addon"><?= l('или менее единиц.') ?></div>
                 </div>
             </td>
@@ -36,11 +36,11 @@
                     <?= l('неснижаемый остаток') ?> <?= InfoPopover::getInstance()->createQuestion('l_good_minimum_balance') ?>
                 </label>
             </td>
-            <td>
+            <td colspan="2">
                 <input placeholder="<?= l('количество товаров') ?>"
                        value="<?= $product['minimum_balance'] ?>"
                        type="text" class="form-control" onkeydown="return isNumberKey(event)"
-                       name="minimum_balance"/>
+                       name="minimum_balance" style="width: 150px;"/>
             </td>
         </tr>
         <tr>
@@ -50,8 +50,8 @@
                     <?= l('Автонаценка') ?> <?= InfoPopover::getInstance()->createQuestion('l_good_automargin') ?>
                 </label>
             </td>
-            <td>
-                <div class="input-group col-sm-7">
+            <td width="30%">
+                <div class="input-group" style="width:150px">
                     <input type="text" class="form-control" value="<?= $product['automargin'] ?>"  style="min-width: 50px" name="automargin"/>
                     <div class="input-group-addon margin-type" onclick="change_margin_type(this, 'automargin')" style="cursor: pointer">
                         <input type="hidden" class="form-control" value="<?= $product['automargin_type'] ?>" name="automargin_type"/>
@@ -59,7 +59,16 @@
                         <span class="percent js-automargin-type"  <?= !$product['automargin_type']? 'style="display:none"':'' ?>>%</span>
                     </div>
                 </div>
-                <div class="input-group col-sm-7" style="margin-top:5px">
+            </td>
+            <td>
+                <?= l('Розница') ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            </td>
+            <td>
+                <div class="input-group" style="width:150px">
                     <input type="text" class="form-control" value="<?= $product['wholesale_automargin'] ?>"  style="min-width: 50px" name="wholesale_automargin"/>
                     <div class="input-group-addon margin-type" onclick="change_margin_type(this, 'wholesale_automargin')" style="cursor: pointer">
                         <input type="hidden" class="form-control" value="<?= $product['wholesale_automargin_type'] ?>" name="wholesale_automargin_type"/>
@@ -67,6 +76,9 @@
                         <span class="percent js-wholesale_automargin-type"  <?= !$product['wholesale_automargin_type']? 'style="display:none"':'' ?>>%</span>
                     </div>
                 </div>
+            </td>
+            <td>
+                <?= l('Опт') ?>
             </td>
         </tr>
         </tbody>
