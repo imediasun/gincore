37b987d39a6a8b678a9553026dafc1ff628f91b5
Igor Lashyn <ilashyn@gmail.com>
2016-10-19 12:45:53 +0300

    fix url for image

--- manage/Views/products/sidebar/_tab_main.php
+++ manage/Views/products/sidebar/_tab_main.php
@@ -36,7 +36,7 @@
             <div class="m-t-sm" id="goods_images">
                 <?php if($images): ?>
                     <?php foreach ($images as $image): ?>
-                    <div class="col-sm-2 m-t-sm"><img class="img-polaroid" src="<?= $this->all_configs['prefix'] ?>/shop/goods/<?= $product['id'] ?>/<?= $image['image'] ?>" width="50px" title=""></div>
+                    <div class="col-sm-2 m-t-sm"><img class="img-polaroid" src="<?= $this->all_configs['siteprefix'] ?>/shop/goods/<?= $product['id'] ?>/<?= $image['image'] ?>" width="50px" title=""></div>
                     <?php endforeach; ?>
                 <?php endif; ?>
 
