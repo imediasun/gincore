1a6457469c8b983459bdd089fd4495fe8fbf9da9
Sergii <khoroshko@gmail.com>
2016-10-20 16:09:28 +0300

    refs #1290
    Изменение цвета кнопки

--- manage/Views/products/sidebar/goods.php
+++ manage/Views/products/sidebar/goods.php
@@ -11,7 +11,7 @@
         <?php include '_tab_suppliers_orders.php';?>
         <?php include '_tab_notification.php';?>
         <div class="form-group m-t-md m-l-md">
-            <input type="submit" class="btn btn-primary" value="<?= l('Сохранить') ?>" id="sidebar-product-form-submit">
+            <input type="submit" class="btn" value="<?= l('Сохранить') ?>" id="sidebar-product-form-submit">
 <!--           <input type="button" class="btn btn-default js_close_sidebar" value="<?= l('Отмена') ?>"> -->
         </div>
     </form>
