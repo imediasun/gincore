90477c786cdb104081a33eec0f013da90247411c
Igor Lashyn <ilashyn@gmail.com>
2016-10-27 15:10:19 +0300

    refs #1261
    кроп названий линков

--- manage/Views/products/supplier_links.php
+++ manage/Views/products/supplier_links.php
@@ -3,7 +3,7 @@
         <p> <?= l('Сайты поставщиков') ?></p>
         <?php if (!empty($links)): ?>
                 <?php foreach ($links as $id => $link): ?>
-                        <p> <?= h($link) ?><a href="<?= $link ?>" target="_blank"> <i class="fa fa-external-link" aria-hidden="true"></i> </a> </p>
+                        <p> <?= cut_string(h($link), 35) ?><a href="<?= $link ?>" target="_blank"> <i class="fa fa-external-link" aria-hidden="true"></i> </a> </p>
                 <?php endforeach; ?>
         <?php endif; ?>
     </div>
