54a8986652034b06e7073b94cff8e8f2def7f294
Sergii <khoroshko@gmail.com>
2016-11-02 17:09:45 +0200

    refs #1111 Перенос кнопки добавление контрагента в форме редактирования пользователя

--- manage/Views/clients/contractors_list.php
+++ manage/Views/clients/contractors_list.php
@@ -1,10 +1,9 @@
 <?php if ($contractors): ?>
-    <div class="<?= !empty($new_client) ? 'span5' : 'col-sm-5' ?>">
+    <div class="<?= !empty($new_client) ? 'span7' : 'col-sm-7' ?>">
         <label class="control-label"><?= l('Контрагент') ?>: </label>
         <?= !empty($infopopover) ? $infopopover : '' ?>
     </div>
-    <div class="<?= !empty($new_client) ? 'span7' : 'col-sm-7' ?>" style="text-align: right; min-width: 150px">
-        <div class="btn-group" role="group" aria-label="...">
+    <div class="<?= !empty($new_client) ? 'span5' : 'col-sm-5' ?> relative" style="text-align: right; min-width: 150px">
             <select name="contractor_id" class="multiselect form-control" id="contractor-select">
                 <option value=""><?= l('Не выбран') ?></option>
                 <?php foreach ($contractors as $contractor): ?>
@@ -16,13 +15,9 @@
             </select>
 
             <?php if(empty($new_client)): ?>
-                <button type="button" title="<?= l('Создать контрагента') ?> "
-                        onclick="add_contractor_from_client_form(this);" class="btn btn-info">
-                    <i class="glyphicon glyphicon-plus"></i>
-                </button>
+                <i class="add_contractor_icon glyphicon glyphicon-plus cursor-pointer" style="position: absolute; right: -11px; top: 10px;" onclick="add_contractor_from_client_form(null);"
+                   title="<?= l('Создать контрагента') ?>"></i>
             <?php endif; ?>
-
-        </div>
     </div>
     <div class="clearfix"></div>
 <?php endif; ?>
