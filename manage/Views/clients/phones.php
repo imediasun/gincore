ef7b7186dc5493b311d7870c727cbc3a73f8b4f5
Sergii <khoroshko@gmail.com>
2016-11-03 17:02:42 +0200

    Добавлен промежуток между полями для ввода телефонов на карточке клиента

--- manage/Views/clients/phones.php
+++ manage/Views/clients/phones.php
@@ -1,6 +1,6 @@
 <?php if (!empty($phones)): ?>
     <?php foreach ($phones as $phone): ?>
-        <input<?=input_phone_mask_attr()?> class="form-control clone_clear_val" type="text" onkeydown="return isNumberKey(event)"
+        <input<?=input_phone_mask_attr()?> class="form-control clone_clear_val m-t-sm" type="text" onkeydown="return isNumberKey(event)"
                name="phone[]" value="<?= htmlspecialchars($phone) ?>"/>
     <?php endforeach; ?>
 <?php else: ?>
