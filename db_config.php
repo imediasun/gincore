9f5fb8e2455782ff99f7000e7ccb8f50be1ec2c4
Igor Lashyn <ilashyn@gmail.com>
2016-11-14 11:23:07 +0200

    загрузка goDB через компосер

--- db_config.php
+++ db_config.php
@@ -14,6 +14,5 @@ if (file_exists(__DIR__ . '/db_config-local.php')) {
     $dbcfg = array_merge($dbcfg, require(__DIR__ . '/db_config-local.php'));
 }
 
-require_once __DIR__ . '/goDB/autoload.php';
-\go\DB\autoloadRegister();
+require_once __DIR__ . '/gincore/bootstrap/autoload.php';
 $db = go\DB\DB::create($dbcfg, 'mysql');
