0a16735e1702798500b22d35bf310d23e7ef68d7
Sergii <khoroshko@gmail.com>
2016-11-15 12:04:49 +0200

    refs #1380
    Добавил внешний ключ чтобы при удалении твоара удалялись и связи с категориями, в миграции так же запрос для фикса у тех кто уже наудалял товаров а связи остались

---
+++ gincore/database/migrations/2016_11_15_085549_add_foreign_categorygoods_goods.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignCategorygoodsGoods extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Illuminate\Support\Facades\DB::statement('DELETE cg FROM `restore4_category_goods` cg LEFT JOIN `restore4_goods` g ON g.id=cg.goods_id WHERE g.id IS NULL;');
+    
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        \Illuminate\Support\Facades\DB::statement('ALTER TABLE `restore4_category_goods` ADD CONSTRAINT `restore4_category_goods_ibfk_2` FOREIGN KEY (`goods_id`) REFERENCES `restore4_goods` (`id`) ON DELETE CASCADE;');
+
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
