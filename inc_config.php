3f13ee7db60d7d415e0dae19fa35ec099c1755e5
Anatoliy K <ragenoir@gmail.com>
2016-11-19 17:51:29 +0200

    Переделал и упростил конфиг для апдейтов

--- inc_config.php
+++ inc_config.php
@@ -8,6 +8,13 @@ date_default_timezone_set('Europe/Kiev');
 $path=str_replace('//', '/', dirname(__FILE__).'/');
 $prefix=str_replace(rtrim($_SERVER['DOCUMENT_ROOT'],'/'), '', $path);
 
-include 'db_config.php';
+$dbcfg = include __DIR__ . '/db_config.php';
+
+if (file_exists(__DIR__ . '/db_config-local.php')) {
+    $dbcfg = array_merge($dbcfg, require(__DIR__ . '/db_config-local.php'));
+}
+
+require_once __DIR__ . '/gincore/bootstrap/autoload.php';
+$db = go\DB\DB::create($dbcfg, 'mysql');
 
 
