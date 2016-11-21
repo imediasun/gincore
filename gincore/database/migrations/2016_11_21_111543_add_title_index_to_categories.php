3bc06ab8eee2b2a20a4e8bcec038a8b77e8bc712
Igor Lashyn <ilashyn@gmail.com>
2016-11-21 13:18:58 +0200

    добавил индекс по полю categories->title

---
+++ gincore/database/migrations/2016_11_21_111543_add_title_index_to_categories.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTitleIndexToCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function ($table) {
+            $table->index('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
