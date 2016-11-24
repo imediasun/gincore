d1d24bdb0772740f58909330634f070d2fa24ee9
Igor Lashyn <ilashyn@gmail.com>
2016-11-24 14:34:34 +0200

    refs #1393
    изменения в редактировании складов

--- manage/Views/warehouses/form_warehouse.php
+++ manage/Views/warehouses/form_warehouse.php
@@ -9,13 +9,19 @@
             <?php endif; ?>
         </a>
     </div>
-    <?php $readonly = !empty($warehouse) && $warehouse['is_system']? 'readonly': '' ?>
+    <?php $readonly = !empty($warehouse) && in_array($warehouse['title'], array(
+                                        lq('Брак'),
+                                        lq('Клиент'),
+                                        lq('Логистика'),
+                                        lq('Недостача'),
+                                    )) ? 'readonly' : '' ?>
     <div id='collapse_warehouse_<?= $i ?>' class='panel-body collapse <?= $i == 1 ? 'in' : '' ?>'>
         <div class='panel-body'>
             <form method='POST'>
                 <div class='form-group'>
                     <label><?= l('Название') ?>: </label>
-                    <input placeholder='<?= l(' введите название') ?>' class='form-control' name='title' <?= $readonly ?>
+                    <input placeholder='<?= l(' введите название') ?>' class='form-control'
+                           name='title' <?= $readonly ?>
                            value='<?= empty($warehouse) ? '' : h($warehouse['title']) ?>' required/>
                 </div>
                 <div class='form-group'>
@@ -24,7 +30,7 @@
                             <input
                                 data-consider='<?= $i ?>' <?= empty($warehouse) || $warehouse['consider_store'] == 1 ? 'checked' : '' ?>
                                 type='checkbox'
-                                onclick='consider(this, "<?= $i ?>")' class='btn consider_<?= $i ?>' <?= $readonly ?>
+                                onclick='consider(this, "<?= $i ?>")' class='btn consider_<?= $i ?>'
                                 name='consider_store' value='1'/>
                             <?= l('Учитывать в свободном остатке') ?>
                         </label>
@@ -33,7 +39,7 @@
                         <label>
                             <input <?= empty($warehouse) || $warehouse['consider_all'] == 1 ? 'checked' : '' ?>
                                 type='checkbox' class='btn consider_<?= $i ?>'
-                                onclick='consider(this, "<?= $i ?>")' name='consider_all' <?= $readonly ?>
+                                onclick='consider(this, "<?= $i ?>")' name='consider_all'
                                 value='1'/>
                             <?= l('Учитывать в общем остатке') ?>
                         </label>
@@ -61,19 +67,18 @@
                 <div class='form-group'>
                     <label>
                         <?= l('Адрес для квитанции') ?>: </label>
-                    <input class='form-control' name='print_address' <?= $readonly ?>
+                    <input class='form-control' name='print_address'
                            value='<?= !empty($warehouse) ? h($warehouse['print_address']) : '' ?>'/>
                 </div>
                 <div class='form-group'>
                     <label> <?= l('Телефон для квитанции') ?>: </label>
-                    <input class='form-control' name='print_phone' <?= $readonly ?>
+                    <input class='form-control' name='print_phone'
                            value='<?= !empty($warehouse) ? h($warehouse['print_phone']) : '' ?>'/>
                 </div>
                 <div class='form-group'>
                     <label><?= l('Локации') ?>: </label>
                     <?= $this->renderFile('warehouses/warehouses_locations', array(
                         'locations' => empty($warehouse['locations']) ? array() : $warehouse['locations'],
-                        'readonly' => $readonly
                     )) ?>
                 </div>
                 <div class='form-group'>
@@ -82,19 +87,7 @@
                             <input type='submit' class='btn' name='warehouse-add' value='<?= l('Создать') ?>'/>
                         <?php else: ?>
                             <input type='hidden' name='warehouse-id' value='<?= $warehouse['id'] ?>'/>
-                            <input type='submit' class='btn' name='warehouse-edit'
-                                <?php if (!$warehouse['can_deleted'] || $warehouse['is_system'] || in_array($warehouse['title'],
-                                        array(
-                                            lq('Брак'),
-                                            lq('Клиент'),
-                                            lq('Логистика'),
-                                            lq('Недостача'),
-                                        ))
-                                ): ?>
-
-                                    onclick="alert('<?= l('Склад не подлежит изменению, так как является системным или задействован в складских и логистических операциях') ?>'); return false"
-                                <?php endif; ?>
-                                   value='<?= l('Редактировать') ?>'/>
+                            <input type='submit' class='btn' name='warehouse-edit' value='<?= l('Редактировать') ?>'/>
                             <input style='margin-left: 10px' type='submit' class='btn' name='warehouse-delete'
                                 <?php if (!$warehouse['can_deleted'] || $warehouse['is_system'] || in_array($warehouse['title'],
                                         array(
@@ -104,7 +97,6 @@
                                             lq('Недостача'),
                                         ))
                                 ): ?>
-
                                     onclick="alert('<?= l('Склад не подлежит удалению, так как задействован в складских и логистических операциях') ?>'); return false"
                                 <?php endif; ?>
                                    value='<?= l('Удалить') ?>'/>
