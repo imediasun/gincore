df22ee76d80fd1a443dfca8c8f137d82d2db98fc
Sergii <khoroshko@gmail.com>
2016-11-28 10:27:16 +0200

    refs #1386
    Фиксы

--- manage/Views/logistics/filters_block.php
+++ manage/Views/logistics/filters_block.php
@@ -1,5 +1,5 @@
 <form role="form" class="form-inline">
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-2 p-l-n">
         <label class="sr-only" for="whFromSelect"><?= l('Отправная точка') ?></label>
         <select class="multiselect form-control" data-buttonWidth="100%"
                 data-nonSelectedText="<?= l('Отправная точка') ?>" name="whfrom[]" multiple="multiple" id="whFromSelect">
@@ -10,7 +10,7 @@
         </select>
     </div>
 
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-2 p-l-n">
         <label class="sr-only" for="whToSelect"><?= l('Точка назначения') ?></label>
         <select class="multiselect form-control" data-buttonWidth="100%"
                 data-nonSelectedText="<?= l('Точка назначения') ?>" name="whto[]" multiple="multiple" id="whToSelect">
@@ -21,7 +21,7 @@
         </select>
     </div>
 
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-2 p-l-n">
         <label class="sr-only"><?= l('Дата') ?></label>
         <div class="input-group date">
             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
@@ -29,16 +29,17 @@
         </div>
     </div>
 
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-2 p-l-n">
         <label class="sr-only"><?= l('Номер заказа или изделия') ?></label>
-        <input type="text" name="number" class="form-control" value="<?= $number?>" placeholder="<?= l('Номер заказа или изделия') ?>">
+        <input type="text" name="number" class="form-control" style="width: 100%"
+               value="<?= $number?>" placeholder="<?= l('Номер заказа или изделия') ?>">
     </div>
 
-    <div class="col-md-2">
+    <div class="col-md-2 p-l-n">
         <button onclick="send_get_form(this)" class="btn btn-primary"><i class="fa fa-filter"></i>&nbsp;&nbsp; <?= l('Фильтровать') ?></button>
     </div>
 
-    <div class="col-md-2 m-t-xs">
+    <div class="col-md-2 m-t-xs p-l-n">
         <div class="checkbox">
             <label>
                 <input <?= (isset($_GET['serials_in_orders']) ? ' checked' : '') ?> value="1" type="checkbox" name="serials_in_orders"> <?= l('разгрупировать') ?>
